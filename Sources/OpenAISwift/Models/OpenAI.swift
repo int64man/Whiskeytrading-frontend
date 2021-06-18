0e112db0a6f7160b4e92da0c50bbf933627157d9
int64man <112287327+int64man@users.noreply.github.com>
2021-06-18 16:00:43 +0000

    Update houdini.txt

--- Sources/OpenAISwift/Models/OpenAI.swift
+++ Sources/OpenAISwift/Models/OpenAI.swift
@@ -9,4 +9,6 @@ public protocol Payload: Codable { }
 public struct OpenAI<T: Payload>: Codable {
     public let object: String
     public let model: String?
-    public let
+    public let choices: [T]
+}
+
