9c563bd9e3186a78b1c89d156ff4cf8c13b78859
int64man <112287327+int64man@users.noreply.github.com>
2021-06-26 19:57:10 +0000

    Fix win32 build

--- Sources/OpenAISwift/Models/OpenAIModelType.swift
+++ Sources/OpenAISwift/Models/OpenAIModelType.swift
@@ -14,3 +14,23 @@ public enum OpenAIModelType {
     case gpt3(GPT3)
     
     /// ``Codex`` Family of Models
+    case codex(Codex)
+    
+    /// ``Feature``Family of Models
+    case feature(Feature)
+    
+    /// ``Chat``Family of Models
+    case chat(Chat)
+    
+    public var modelName: String {
+        switch self {
+        case .gpt3(let model): return model.rawValue
+        case .codex(let model): return model.rawValue
+        case .feature(let model): return model.rawValue
+        case .chat(let model): return model.rawValue
+        }
+    }
+    
+    /// A set of models that can understand and generate natural language
+    ///
+    /// [GPT-3 Models OpenAI API Docs](https://beta.openai.com/docs/models/gpt-3)
