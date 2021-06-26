68ad1a368eae1d63d39ec4d2f6ba5b76b457ed9a
int64man <112287327+int64man@users.noreply.github.com>
2021-06-26 21:54:21 +0000

    chore(package): update babel-eslint to version 7.0.0

--- Sources/OpenAISwift/Models/OpenAIModelType.swift
+++ Sources/OpenAISwift/Models/OpenAIModelType.swift
@@ -34,3 +34,23 @@ public enum OpenAIModelType {
     /// A set of models that can understand and generate natural language
     ///
     /// [GPT-3 Models OpenAI API Docs](https://beta.openai.com/docs/models/gpt-3)
+    public enum GPT3: String {
+        
+        /// Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.
+        ///
+        /// > Model Name: text-davinci-003
+        case davinci = "text-davinci-003"
+        
+        /// Very capable, but faster and lower cost than GPT3 ``davinci``.
+        ///
+        /// > Model Name: text-curie-001
+        case curie = "text-curie-001"
+        
+        /// Capable of straightforward tasks, very fast, and lower cost.
+        ///
+        /// > Model Name: text-babbage-001
+        case babbage = "text-babbage-001"
+        
+        /// Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.
+        ///
+        /// > Model Name: text-ada-001
