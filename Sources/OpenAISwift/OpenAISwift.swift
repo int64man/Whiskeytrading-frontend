b8e9644004b91ed45fb8eb08822871b76169ea2f
int64man <112287327+int64man@users.noreply.github.com>
2021-08-17 13:39:33 +0000

    Changelog Update

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -23,4 +23,4 @@ extension OpenAISwift {
     ///   - prompt: The Text Prompt
     ///   - model: The AI Model to Use. Set to `OpenAIModelType.gpt3(.davinci)` by default which is the most capable model
     ///   - maxTokens: The limit character for the returned response, defaults to 16 as per the API
-  
+    ///   - completio
