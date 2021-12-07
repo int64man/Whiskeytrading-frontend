e2616f52b59375d7c2c0d762d49160e09be43079
int64man <112287327+int64man@users.noreply.github.com>
2021-12-07 13:58:40 +0000

    code review fix

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -62,4 +62,4 @@ extension OpenAISwift {
                     let res = try JSONDecoder().decode(OpenAI<TextResult>.self, from: success)
                     completionHandler(.success(res))
                 } catch {
-                    com
+                    completionHandler(.failur
