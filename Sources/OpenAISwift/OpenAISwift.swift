44ef830104217dcf1f293d23b0c91f194586c899
int64man <112287327+int64man@users.noreply.github.com>
2022-03-31 08:04:26 +0000

    add route

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -79,4 +79,4 @@ extension OpenAISwift {
     ///   - completionHandler: Returns an OpenAI Data Model
     public func sendChat(with messages: [ChatMessage], model: OpenAIModelType = .chat(.chatgpt), maxTokens: Int? = nil, temperature: Double = 1.0, completionHandler: @escaping (Result<OpenAI<MessageResult>, OpenAIError>) -> Void) {
         let endpoint = Endpoint.chat
-        let body = ChatConversation(messages: messages, model: model.modelName, maxTokens: max
+        let body = ChatConversation(messages: messages, model: model.modelName, maxTokens: maxTokens, temperature: tempe
