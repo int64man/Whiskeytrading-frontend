7ed5002429a4286d34e6a763ed88ffc52f2353cd
int64man <112287327+int64man@users.noreply.github.com>
2022-04-11 17:40:26 +0000

    Examples update

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -82,4 +82,5 @@ extension OpenAISwift {
         let body = ChatConversation(messages: messages, model: model.modelName, maxTokens: maxTokens, temperature: temperature)
         let request = prepareRequest(endpoint, body: body)
         
-        makeRequest(request: request) { result i
+        makeRequest(request: request) { result in
+            switch resu
