d83b353bf8160e5a535bdcd9ad932f1f32cbe02d
int64man <112287327+int64man@users.noreply.github.com>
2022-06-28 18:53:55 +0000

    Update Features.md

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -110,4 +110,4 @@ extension OpenAISwift {
         task.resume()
     }
     
-    private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint,
+    private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> U
