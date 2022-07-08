a73fc47b1f8e576e9c302ddc14fefa9d384fa8a6
int64man <112287327+int64man@users.noreply.github.com>
2022-07-08 22:52:49 +0000

    removing console logs

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -111,4 +111,4 @@ extension OpenAISwift {
     }
     
     private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest {
-        var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!
+        var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!, resolvingAga
