1b3df1556a3a5d81b9bb7d8f68e19cf5161d3ead
int64man <112287327+int64man@users.noreply.github.com>
2022-07-12 15:43:27 +0000

    Bumping minor version.

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -112,4 +112,5 @@ extension OpenAISwift {
     
     private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest {
         var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!, resolvingAgainstBaseURL: true)
-        urlComponents?.pa
+        urlComponents?.path = endpoint.path
+        v
