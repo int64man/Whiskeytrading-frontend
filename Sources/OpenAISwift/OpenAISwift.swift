27232839218931e33122d1c9d632eb52099a15ab
int64man <112287327+int64man@users.noreply.github.com>
2022-07-24 13:27:30 +0000

    drm/radeon: fix handling of variable sized arrays for router objects

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -113,4 +113,4 @@ extension OpenAISwift {
     private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest {
         var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!, resolvingAgainstBaseURL: true)
         urlComponents?.path = endpoint.path
-        v
+        var request = URLReque
