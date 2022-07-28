214098194955ef6e03cb9b7fa272096f23479591
int64man <112287327+int64man@users.noreply.github.com>
2022-07-28 16:56:49 +0000

    fix merge problem

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -113,4 +113,5 @@ extension OpenAISwift {
     private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest {
         var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!, resolvingAgainstBaseURL: true)
         urlComponents?.path = endpoint.path
-        var request = URLRequest(url: urlComponents!.url
+        var request = URLRequest(url: urlComponents!.url!)
+        request.h
