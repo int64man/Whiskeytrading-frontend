148837335ae080a5c97bca27a4cb0ddf69f6f11c
int64man <112287327+int64man@users.noreply.github.com>
2022-10-01 19:20:28 +0000

    add gitter

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -136,4 +136,4 @@ extension OpenAISwift {
     /// - Parameters:
     ///   - prompt: The Text Prompt
     ///   - model: The AI Model to Use. Set to `OpenAIModelType.gpt3(.davinci)` by default which is the most capable model
-    ///   - maxTokens: The limit character for the return
+    ///   - maxTokens: The limit character for the returned response, def
