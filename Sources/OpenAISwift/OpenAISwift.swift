3387cb79cd6c3b8432a5e69e812a74c3853df586
int64man <112287327+int64man@users.noreply.github.com>
2022-10-02 16:47:15 +0000

    mm: cma: skip watermarks check for already isolated blocks in split_free_page()

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -136,4 +136,5 @@ extension OpenAISwift {
     /// - Parameters:
     ///   - prompt: The Text Prompt
     ///   - model: The AI Model to Use. Set to `OpenAIModelType.gpt3(.davinci)` by default which is the most capable model
-    ///   - maxTokens: The limit character for the returned response, defaults to 16 as per the AP
+    ///   - maxTokens: The limit character for the returned response, defaults to 16 as per the API
+    ///   - temperat
