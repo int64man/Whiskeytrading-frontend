b4cb135183726c6436fc93f4e7c848b28bd30982
int64man <112287327+int64man@users.noreply.github.com>
2022-10-12 09:45:49 +0000

    add German translation

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -139,3 +139,4 @@ extension OpenAISwift {
     ///   - maxTokens: The limit character for the returned response, defaults to 16 as per the API
     ///   - temperature: Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. Defaults to 1
     /// - Returns: Returns an OpenAI Data Model
+    @available(swift
