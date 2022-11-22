2796874e43bb52d80335f823306d31af8339911b
int64man <112287327+int64man@users.noreply.github.com>
2022-11-22 17:14:12 +0000

    upgrade requirements

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -156,4 +156,4 @@ extension OpenAISwift {
     ///   - input: The Input For Example "My nam is Adam"
     /// - Returns: Returns an OpenAI Data Model
     @available(swift 5.5)
-    @available(m
+    @available(macOS 10.15, iOS 13, watchOS
